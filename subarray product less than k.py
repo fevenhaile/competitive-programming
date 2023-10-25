932aa73768760534a5d15fb93877af88d9281ed6
fevenhaile <115413240+fevenhaile@users.noreply.github.com>
2023-10-25 23:56:33 +0300

    added subarray product less than k.py

---
+++ subarray product less than k.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numSubarrayProductLessThanK(self, nums, k):
+        l=0
+        res=0
+        product=1
+
+        for r in range(len(nums)):
+            product*=nums[r]
+
+            if product >= k:
+                while product >= k and l<=r:
+                    product/=nums[l]
+                    l+=1
+            res+=r-l+1
+        return res
