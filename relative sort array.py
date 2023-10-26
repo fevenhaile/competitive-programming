480c5887b035bbc292dfbe5fa6c22302ec0b4339
fevenhaile <115413240+fevenhaile@users.noreply.github.com>
2023-10-26 17:54:54 +0300

    added relative sort array.py

---
+++ relative sort array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def relativeSortArray(self, arr1, arr2):
+        
+        lookup = {v: i for i, v in enumerate(arr2)}class Solution(object):
+    def relativeSortArray(self, arr1, arr2):
+        
+        lookup = {v: i for i, v in enumerate(arr2)}
+        return sorted(arr1, key=lambda i: lookup.get(i, len(arr2)+i))
+        return sorted(arr1, key=lambda i: lookup.get(i, len(arr2)+i))
