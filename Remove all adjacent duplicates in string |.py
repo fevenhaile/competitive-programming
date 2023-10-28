878c06c563ade872b3cead9b270894e4f8920e50
fevenhaile <115413240+fevenhaile@users.noreply.github.com>
2023-10-28 15:48:10 +0300

    added Remove all adjacent duplicates in string |.py

---
+++ Remove all adjacent duplicates in string |.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeDuplicates(self, S):
+        stack = []
+        for char in S:
+            if stack and stack[-1] == char:
+                stack.pop()
+            else:
+                stack.append(char)
+        return ''.join(stack)
