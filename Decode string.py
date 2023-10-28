a7e1432b88bc9c0c171530aab2de4e61acbb837e
fevenhaile <115413240+fevenhaile@users.noreply.github.com>
2023-10-28 15:57:39 +0300

    added Decode string.py

---
+++ Decode string.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def decodeString(self, s):
+        stack = []
+        curr_num = 0
+        curr_str = ''
+        
+        for char in s:
+            if char.isdigit():
+                curr_num = curr_num * 10 + int(char)
+            elif char == '[':
+                stack.append(curr_str)
+                stack.append(curr_num)
+                curr_str = ''
+                curr_num = 0
+            elif char == ']':
+                num = stack.pop()
+                prev_str = stack.pop()
+                curr_str = prev_str + num * curr_str
+            else:
+                curr_str += char
+        
+        return curr_str
