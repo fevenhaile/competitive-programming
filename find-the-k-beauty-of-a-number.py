f1d747be053ec836cbd07d63dcec5f9fe505e102
fevenhaile <115413240+fevenhaile@users.noreply.github.com>
2023-11-14 13:06:48 +0300

    added find-the-k-beauty-of-a-number.python

--- find-the-k-beauty-of-a-number.py
+++ find-the-k-beauty-of-a-number.py
@@ -1,10 +1,10 @@
 class Solution:
     def divisorSubstrings(self, num: int, k: int) -> int:
-        s = str(num)
-        counter = 0
+        s=str(num)
+        counter=0
+        
         for i in range(len(s)-k+1):
-            x =  int(s[i:i+k])
-            if x and num%x == 0:
-                # print(x)
-                counter += 1
+            casted=int(s[i:i+k])
+            if casted and num % casted == 0:
+                counter+=1
         return counter
