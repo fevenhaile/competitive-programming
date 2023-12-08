8c4b56983b98284ebbda1aa3627e7ce72a1e33e2
fevenhaile <115413240+fevenhaile@users.noreply.github.com>
2023-12-08 22:30:33 +0300

    Added solution for Minimum Time Visiting All Points

---
+++ A2SV/week2/minimum-time-visiting-all-points.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        totaltime = 0
+
+        for i in range(len(points) - 1):
+            x_diff = abs(points[i+1][0] - points[i][0])
+            y_diff = abs(points[i+1][1] - points[i][1])
+            totaltime += max(x_diff, y_diff)
+
+        return totaltime
