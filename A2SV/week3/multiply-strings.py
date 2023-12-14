145691ce0e8400a6a27404bdca70e4a358599e46
fevenhaile <115413240+fevenhaile@users.noreply.github.com>
2023-12-14 10:00:42 +0300

    Added solution for Multiply Strings

---
+++ A2SV/week3/multiply-strings.py
@@ -0,0 +1,6 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        type_casted = int (num1)
+        type_casted2 = int(num2)
+        return str(type_casted*type_casted2)
+        
