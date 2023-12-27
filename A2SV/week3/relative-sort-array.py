daaf3bcd5154779df5c452a4eb7b63e92e6e93ab
fevenhaile <115413240+fevenhaile@users.noreply.github.com>
2023-12-27 18:54:57 +0300

    Added solution for Relative Sort Array

---
+++ A2SV/week3/relative-sort-array.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def relativeSortArray(self, arr1, arr2):
+        
+        lookup = {v: i for i, v in enumerate(arr2)}
+        return sorted(arr1, key=lambda i: lookup.get(i, len(arr2)+i))
