0b1a8abda88b33ca6fa6df4c8ad38b355335be37
fevenhaile <115413240+fevenhaile@users.noreply.github.com>
2024-01-03 16:01:24 +0300

    Added solution for Move Zeroes

--- A2SV/week3/move-zeroes.py
+++ A2SV/week3/move-zeroes.py
@@ -1,17 +1,17 @@
-class Solution(object):
-    def moveZeroes(self, nums):
-        l=r=0
-        n=len(nums)
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+        r = 0
+        n = len(nums)
+        while r < n:
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+            r += 1
+            
 
-        if n<2:
-            return
-        while r<n:
-            if nums[l]!=0:
-                l+=1
-                r+=1
-            elif nums[r]==0:
-                r+=1
-            else:
-                temp=nums[l]
-                nums[l]=nums[r]
-                nums[r]=temp    
+
+        
