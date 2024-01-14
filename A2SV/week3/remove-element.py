c16050927051393dda7191d3aaed9de775721870
fevenhaile <115413240+fevenhaile@users.noreply.github.com>
2024-01-14 23:33:56 +0300

    Added solution for Remove Element

---
+++ A2SV/week3/remove-element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        l = 0
+        while l < len(nums):
+            if nums[l] == val:
+                nums.pop(l)
+            else:
+                l += 1
+                
+        return l
