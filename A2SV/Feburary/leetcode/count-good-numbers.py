8712f161098d66e30642de59c2328eb94e0a90fa
fevenhaile <115413240+fevenhaile@users.noreply.github.com>
2024-02-26 17:04:43 +0300

    Add solution for Count Good Numbers

--- A2SV/Feburary/leetcode/count-good-numbers.py
+++ A2SV/Feburary/leetcode/count-good-numbers.py
@@ -2,6 +2,14 @@ class Solution:
     def countGoodNumbers(self, n: int) -> int:
         if n == 1:
             return 5
+        @cache
+        def pow(x, y, mod):
+            if y == 1:
+                return x
+            num = pow(x,y//2,mod)
+            if y % 2 == 0:
+                return (num * num) % mod
+            return (num * num * x) % mod
         x = pow(5,n//2, (10**9)+7)
         y = pow(4,n//2, (10**9)+7)
         if n % 2 == 0:
